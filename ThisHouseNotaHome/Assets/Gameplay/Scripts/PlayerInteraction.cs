99c26893317db471871916c4c8ecd8e3a6332f5e
glammetalgamer <glammetalgamer@gmail.com>
2019-01-27 14:08:19 -0500

    a

--- ThisHouseNotaHome/Assets/Gameplay/Scripts/PlayerInteraction.cs
+++ ThisHouseNotaHome/Assets/Gameplay/Scripts/PlayerInteraction.cs
@@ -9,16 +9,34 @@ public class PlayerInteraction : MonoBehaviour {
     private PlayerMovement playerMovementComponent;
     [SerializeField]
     private float interactionDistance;
+	[SerializeField]
+	private AudioSource audioSource;
+	public AudioClip bush;
+
 
     // Start is called before the first frame update
     void Start() {}
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetButtonDown(interactionInput)) {
+      /*  if (Input.GetButtonDown(interactionInput)) {
             interact();
-        }
+        }*/
     }
+
+
+	void OnCollisionEnter2D(Collision2D other){
+		Collectible collectible = other.gameObject.GetComponent<Collectible>();
+		if (collectible != null) {
+			ItemTracker.instance.FoundItem (collectible);
+		}
+
+		if (other.gameObject.tag == "Bush")
+			audioSource.PlayOneShot (bush);
+
+	}
+
+
 
     private void interact() {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, playerMovementComponent.GetDirection().normalized, interactionDistance);
