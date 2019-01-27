3b05e8d3571d7433fd3e0203e6f1ab26a92f3d10
Ali Onque <ali@randomwhale.com>
2019-01-27 14:21:10 -0500

    Result Scene and Project Settings Update

---
+++ ThisHouseNotaHome/Assets/Gameplay/Scripts/Results.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Results : MonoBehaviour {
+    [SerializeField]
+    private GameObject[] itemImages;
+    // Start is called before the first frame update
+    void Start() {
+        if(ItemTracker.instance == null) {
+            return;
+        }
+
+        int[] collectedItemIds = ItemTracker.instance.GetFoundItems().ToArray();
+
+        foreach(int i in collectedItemIds) {
+            itemImages[i - 1].SetActive(true);
+        }
+
+        Destroy(ItemTracker.instance.gameObject);
+        Destroy(this.gameObject);
+    }
+}
